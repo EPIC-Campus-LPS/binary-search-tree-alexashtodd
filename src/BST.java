28eb1de8d91bc62021734afd724a4b02568167f6
alexashtodd <alexashtodd@gmail.com>
2026-04-07 15:05:03 -0600

    302

--- src/BST.java
+++ src/BST.java
@@ -1,4 +1,4 @@
-
+import com.sun.source.tree.Tree;
 
 public class BST<E extends Comparable<E>> {
     private TreeNode<E> root;
@@ -7,9 +7,11 @@ public class BST<E extends Comparable<E>> {
         if(root == null){
             root = new TreeNode<>(value, null, null);
         }else{
-            if()
+            TreeNode<E> temp = GreaterlessChoser(root, new TreeNode<>(value, null, null));
+            while(!testForLeafNode()){
+                temp = GreaterlessChoser(temp,new TreeNode<>(value, null, null));
+            }
         }
-
     }
     boolean contains(E value){
 
@@ -21,6 +23,27 @@ public class BST<E extends Comparable<E>> {
     void printPreorder(){}
     void printPostorder(){}
     E delete(E value){}
+
+    boolean testForLeafNode(){
+        TreeNode<E> temp = root;
+        TreeNode<E> A = root.getLeftChild();
+        TreeNode<E> B = root.getRightChild();
+        if(A == null && B == null){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    TreeNode<E> GreaterlessChoser(TreeNode<E> Ancor,TreeNode<E> Value){
+        int root1 = Ancor.compareTo(Value);
+        TreeNode<E> temp = null;
+        if(root1<0){
+             temp = root.getLeftChild();
+        } else if (root1>0) {
+             temp = root.getRightChild();
+        }
+        return temp;
+    }
 
 
     String IntOrString(E value){
